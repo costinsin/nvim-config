f35d4f42bac6cb53e02408942d83114f715c3da9
Costin Sin <sin.costinrobert@gmail.com>
2024-02-15 09:50:42 +0200

    Initial commit
    Signed-off-by: Costin Sin <sin.costinrobert@gmail.com>

---
+++ lua/config/options.lua
@@ -0,0 +1,17 @@
+-- WSL Clipboard support
+if vim.fn.has("wsl") == 1 then
+  -- This is NeoVim's recommended way to solve clipboard sharing if you use WSL
+  -- See: https://github.com/neovim/neovim/wiki/FAQ#how-to-use-the-windows-clipboard-from-wsl
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
